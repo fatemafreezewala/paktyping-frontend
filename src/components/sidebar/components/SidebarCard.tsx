be7d6f936e430a56a6a06a93ebdc87d627ba7e82
Fatema Freezzy <fatemafreezzy@Fatemas-Mac-mini.local>
2025-01-12 16:13:54 +0530

    solved2

--- src/components/sidebar/components/SidebarCard.tsx
+++ src/components/sidebar/components/SidebarCard.tsx
@@ -1,4 +1,4 @@
-import { Flex, useColorModeValue } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
 export default function SidebarDocs() {
   const bgColor = 'linear-gradient(135deg, #868CFF 0%, #4318FF 100%)';
